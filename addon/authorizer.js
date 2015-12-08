5b4e91d4301dcab5e65ac17b9297455c10156426
Darragh O'Keeffe <hey@darraghokeeffe.com>
2015-12-08 17:49:35 +0000

    added authorizer add on

--- addon/authorizer.js
+++ addon/authorizer.js
@@ -2,11 +2,10 @@ import authorizerBase from 'ember-simple-auth/authorizers/base';
 import Ember from 'ember';
 
 export default authorizerBase.extend({
-  authorize: function(jqXHR) {
-    var accessToken = this.get('session.access_token');
-
-    if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
-      jqXHR.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+  authorize(data, block) {
+    const accessToken = data['access_token'];
+    if (!Ember.isEmpty(accessToken)) {
+      block('Authorization', `Bearer ${accessToken}`);
     }
   }
 });
