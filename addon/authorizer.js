14021bff3ef0eeb8bda4eb7dccbaa15780b6a21e
Chris Manson <chris@stonecircle.io>
2018-04-26 20:25:37 +0100

    run codemods and remove .fmt()

--- addon/authorizer.js
+++ addon/authorizer.js
@@ -1,10 +1,10 @@
+import { isEmpty } from '@ember/utils';
 import authorizerBase from 'ember-simple-auth/authorizers/base';
-import Ember from 'ember';
 
 export default authorizerBase.extend({
   authorize(data, block) {
     const accessToken = data['access_token'];
-    if (!Ember.isEmpty(accessToken)) {
+    if (!isEmpty(accessToken)) {
       block('Authorization', `Bearer ${accessToken}`);
     }
   }
