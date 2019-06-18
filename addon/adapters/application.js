bc72a216c778c1a1e68714a724821d094b2491d1
Chris Manson <chris@manson.ie>
2019-06-18 18:24:13 +0100

    fixing missing authorizer

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -2,4 +2,5 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:application'
 });
