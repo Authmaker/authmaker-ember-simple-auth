719962825bf1f45cefab35a44f9849593dded73b
Chris Manson <chris@manson.ie>
2022-01-24 22:51:12 +0000

    fix incorrect use of ember-window-mock

--- tests/dummy/app/controllers/fake-oauth/signin.js
+++ tests/dummy/app/controllers/fake-oauth/signin.js
@@ -5,7 +5,7 @@ import window from 'ember-window-mock';
 export default Controller.extend({
   actions: {
     login() {
-      window.location = 'http://localhost:4200/login#something'
+      window.location.href = 'http://localhost:4200/login#something'
     }
   }
 });
